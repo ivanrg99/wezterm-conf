2fe214977503d18874960888c023525e417d822c
Ivan <ivan@infinitefieldtrading.com>
2025-01-03 14:44:24 +0100

    change

--- wezterm.lua
+++ wezterm.lua
@@ -179,51 +179,51 @@ config.keys = {
 
 
 
-config.keys = {{
+-- config.keys = {{
 
-    key = "1",
-    mods = "ALT",
-    action = wezterm.action.ActivateTab(0)
-}, {
-    key = "2",
-    mods = "ALT",
-    action = wezterm.action.ActivateTab(1)
-}, {
-    key = "3",
-    mods = "ALT",
-    action = wezterm.action.ActivateTab(2)
-}, {
-    key = "4",
-    mods = "ALT",
-    action = wezterm.action.ActivateTab(3)
-}, {
-    key = "5",
-    mods = "ALT",
-    action = wezterm.action.ActivateTab(4)
-}, {
-    key = "6",
-    mods = "ALT",
-    action = wezterm.action.ActivateTab(5)
-}, {
-    key = "7",
-    mods = "ALT",
-    action = wezterm.action.ActivateTab(6)
-}, {
-    key = "8",
-    mods = "ALT",
-    action = wezterm.action.ActivateTab(7)
-}, {
-    key = "9",
-    mods = "ALT",
-    action = wezterm.action.ActivateTab(8)
-}, {
-    key = "0",
-    mods = "ALT",
-    action = wezterm.action.ActivateTab(9)
-}, {
-    key = "t",
-    mods = "ALT",
-    action = wezterm.action.SpawnTab("CurrentPaneDomain")
-}}
+--     key = "1",
+--     mods = "ALT",
+--     action = wezterm.action.ActivateTab(0)
+-- }, {
+--     key = "2",
+--     mods = "ALT",
+--     action = wezterm.action.ActivateTab(1)
+-- }, {
+--     key = "3",
+--     mods = "ALT",
+--     action = wezterm.action.ActivateTab(2)
+-- }, {
+--     key = "4",
+--     mods = "ALT",
+--     action = wezterm.action.ActivateTab(3)
+-- }, {
+--     key = "5",
+--     mods = "ALT",
+--     action = wezterm.action.ActivateTab(4)
+-- }, {
+--     key = "6",
+--     mods = "ALT",
+--     action = wezterm.action.ActivateTab(5)
+-- }, {
+--     key = "7",
+--     mods = "ALT",
+--     action = wezterm.action.ActivateTab(6)
+-- }, {
+--     key = "8",
+--     mods = "ALT",
+--     action = wezterm.action.ActivateTab(7)
+-- }, {
+--     key = "9",
+--     mods = "ALT",
+--     action = wezterm.action.ActivateTab(8)
+-- }, {
+--     key = "0",
+--     mods = "ALT",
+--     action = wezterm.action.ActivateTab(9)
+-- }, {
+--     key = "t",
+--     mods = "ALT",
+--     action = wezterm.action.SpawnTab("CurrentPaneDomain")
+-- }}
 
 return config
